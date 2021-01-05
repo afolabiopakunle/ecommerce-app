ecb1f93775745cf9dd4c26cb40bdafe67785b250
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-01-05 19:03:38 +0100

    cart total working

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -12,12 +12,18 @@ export class CartComponent implements OnInit {
     {id: 2, productName: "Product F", qty: 6, price: 1800},
     {id: 3, productName: "Product E",  qty: 1, price: 100},
     {id: 4, productName: "Product D",  qty: 2, price: 1100},
-    {id: 5, productName: "Product B",  qty: 1, price: 1200},
+    {id: 5, productName: "Product B",  qty: 3, price: 1200},
   ];
+
+
+  cartTotal = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.cartItems.forEach(item => {
+      this.cartTotal += (item.price * item.qty)
+    })
   }
 
 }
